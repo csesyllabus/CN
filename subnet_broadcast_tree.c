d6d7af73e7f89b50016678154f0691525ef963a2
cse syllabus <135509159+csesyllabus@users.noreply.github.com>
2024-02-29 07:51:02 +0530

    Update subnet_broadcast_tree.c

--- subnet_broadcast_tree.c
+++ subnet_broadcast_tree.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
-void main()
+
+int main()
 {
     unsigned int compad[4];
     unsigned int mask[4];
     unsigned int netaddr[4];
-    int i, n;
-    printf("enter the ip address \n");
-    scanf("%u%*c%u%*c%u%*c%u%*c", &compad[3], &compad[2], &compad[1], &compad[0]);
-    printf("enter the subnet address:");
-    scanf("%u%*c%u%*c%u%*c%u%*c", &mask[3], &mask[2], &mask[1], &mask[0]);
-    for (i = 0; i < n; i++)
+    int i;
+
+    printf("Enter the IP address: \n");
+    scanf("%u.%u.%u.%u", &compad[3], &compad[2], &compad[1], &compad[0]);
+    printf("Enter the subnet mask: ");
+    scanf("%u.%u.%u.%u", &mask[3], &mask[2], &mask[1], &mask[0]);
+
+    for (i = 0; i < 4; i++)
     {
         netaddr[i] = compad[i] & mask[i];
     }
-    printf("\n network address is :\n");
-    printf("%u%u%u%u", netaddr[3], netaddr[2], netaddr[1], netaddr[0]);
-    printf("subnet address is:\n");
-    printf("%u%u%u%u", mask[3], mask[2], mask[1], mask[0]);
-    printf("ip address is :\n");
-    printf("%u%u%u%u", compad[3], compad[2], compad[1], compad[0]);
+
+    printf("\nNetwork address is :\n");
+    printf("%u.%u.%u.%u\n", netaddr[3], netaddr[2], netaddr[1], netaddr[0]);
+
+    printf("Subnet mask is:\n");
+    printf("%u.%u.%u.%u\n", mask[3], mask[2], mask[1], mask[0]);
+
+    printf("IP address is :\n");
+    printf("%u.%u.%u.%u\n", compad[3], compad[2], compad[1], compad[0]);
+
+    return 0;
 }
+/*
+OUTPUT
+
+Enter the IP address:
+192.168.101.226
+Enter the subnet mask: 255.255.255.8
+
+Network address is :
+192.168.101.0
+Subnet mask is:
+255.255.255.8
+IP address is :
+192.168.101.226
+*/
