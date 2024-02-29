f7597033dfdcd852273e9357d5aacef4f5be3c07
cse syllabus <135509159+csesyllabus@users.noreply.github.com>
2024-02-29 07:50:43 +0530

    Update leaky_bucket.c

--- leaky_bucket.c
+++ leaky_bucket.c
@@ -1,38 +1,44 @@
 // leaky bucket
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int incoming, outgoing, bucket_size, n, store = 0;
-    
+
     printf("Enter the bucket size, outgoing rate, and number of inputs: ");
     scanf("%d %d %d", &bucket_size, &outgoing, &n);
-    
-    while (n > 0) {
+
+    while (n > 0)
+    {
         printf("Enter the incoming packet size: ");
         scanf("%d", &incoming);
         printf("Incoming packet size is %d\n", incoming);
-        
-        if (incoming <= (bucket_size - store)) {
+
+        if (incoming <= (bucket_size - store))
+        {
             store += incoming;
             printf("Bucket buffer size %d out of %d\n", store, bucket_size);
-        } else {
+        }
+        else
+        {
             int overflow = incoming - (bucket_size - store);
             printf("Dropped %d packets\n", overflow);
             store = bucket_size; // Bucket is full
             printf("Bucket buffer %d out of %d\n", store, bucket_size);
         }
-        
+
         store -= outgoing;
         if (store < 0)
             store = 0; // Bucket cannot have negative packets
-        
+
         printf("After outgoing %d packets left out of %d in buffer\n", store, bucket_size);
-        
+
         n--;
     }
-    
+
     return 0;
 }
+
 /*
 output :-
 enter the bucket size,outgoing, rate no of inputs: 50 6 60
